ed87c89dc18ec94e5ee04f4654aae1addadc642f
Christoffer Åhrling <christoffer@nilpath.se>
2020-10-09 15:35:14 +0200

    Add PositionwiseFeedForward layer

---
+++ src/keras_transformer/feedforward.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+class PositionwiseFeedForward(tf.keras.layers.Layer):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.d1 = tf.keras.layers.Dense(d_ff)
+        self.relu = tf.keras.layers.Activation('relu')
+        self.d2 = tf.keras.layers.Dense(d_model)
+        self.dropout = tf.keras.layers.Dropout(dropout)
+
+    def call(self, x):
+        x = self.d1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.d2(x)
+        return x
