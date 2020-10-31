fix masking

src/keras_transformer/model.py
@@ -47,8 +47,11 @@ class TransformerModel(tf.keras.Model):
         src, tgt = data
         tgt_in = tgt[:, :-1]
         tgt_out = tgt[:, 1:]
+
         src_mask = create_padding_mask(src, 0)
-        tgt_mask = create_look_ahead_mask(tf.shape(tgt_in)[1])
+        tgt_pad_mask = create_padding_mask(tgt_in, 0)
+        tgt_look_ahead_mask = create_look_ahead_mask(tf.shape(tgt_in)[1])
+        tgt_mask = tf.maximum(tgt_pad_mask, tgt_look_ahead_mask)
 
         with tf.GradientTape() as tape:
             y_pred = self([src, tgt_in, src_mask, tgt_mask], training=True)

src/keras_transformer/transformer.py
@@ -36,9 +36,9 @@ class Encoder(tf.keras.layers.Layer):
         self.layers = [copy.deepcopy(layer) for _ in range(N)]
         self.norm = LayerNorm(layer.size)
 
-    def call(self, x, masks):
+    def call(self, x, mask):
         for layer in self.layers:
-            x = layer(x, masks)
+            x = layer(x, mask)
         return self.norm(x)  # (batch_size, input_seq_len, d_model)
 
 
@@ -57,9 +57,9 @@ class EncoderLayer(tf.keras.layers.Layer):
         self.norm2 = LayerNorm(d_model)
         self.dropout2 = tf.keras.layers.Dropout(dropout)
 
-    def call(self, x, masks):
+    def call(self, x, mask):
         x_norm = self.norm1(x)
-        out = self.attention(x_norm, x_norm, x_norm, masks)
+        out = self.attention(x_norm, x_norm, x_norm, mask)
         x = x + self.dropout1(out)
 
         x_norm = self.norm2(x)

tests/test_masking.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from keras_transformer.masking import create_look_ahead_mask
+from keras_transformer.masking import create_look_ahead_mask, create_padding_mask
 
 
 class CreateLookAheadMaskTest(tf.test.TestCase):
@@ -13,4 +13,13 @@ class CreateLookAheadMaskTest(tf.test.TestCase):
             ]
         )
         mask = create_look_ahead_mask(3)
+        self.assertAllEqual(mask, expected_mask)
+
+
+class CreatePaddingMaskTest(tf.test.TestCase):
+    def test_create_mask(self):
+        expected_mask = tf.constant(
+            [[[[0, 0, 0, 1, 1, 1]]]]
+        )
+        mask = create_padding_mask(tf.constant([[1, 2, 3, 0, 0, 0]]))
         self.assertAllEqual(mask, expected_mask)